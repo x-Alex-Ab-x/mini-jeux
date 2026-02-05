93d999a9bdcc01b8e1ed7f2a764478295dc98e30
x-Alex-Ab-x <alexandra_abra@hotmail.com>
2026-02-05 16:52:43 -0500

    Fix background music control

--- js/index_music.js
+++ js/index_music.js
@@ -1,23 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const btn = document.getElementById("btn-music");
-    const audio = document.getElementById("bg-music");
+const music = document.getElementById("bg-music");
+const btn = document.getElementById("btn-music");
 
-    let playing = false;
-
-    btn.addEventListener("click", () => {
-        if (!playing) {
-            audio.play()
-                .then(() => {
-                    playing = true;
-                    btn.textContent = "⏸️ Mettre la musique en pause";
-                })
-                .catch((error) => {
-                    console.error("La lecture automatique a échoué :", error);
-                });
-        } else {
-            audio.pause();
-            playing = false;
-            btn.textContent = "🎵 Rejouer la musique";
-        }
-    });
+btn.addEventListener("click", () => {
+    if (music.paused) {
+    music.play();
+    btn.textContent = "⏸️ Pause musique";
+    } else {
+    music.pause();
+    btn.textContent = "🎵 Lancer la musique";
+    }
+});
 });
