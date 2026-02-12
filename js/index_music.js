8ce0184afb1076ef92edd3f7e61cfa10954b3cd0
x-Alex-Ab-x <alexandra_abra@hotmail.com>
2026-02-12 13:56:01 -0500

    Lower default music volume

--- js/index_music.js
+++ js/index_music.js
@@ -2,8 +2,8 @@ window.addEventListener("DOMContentLoaded", () => {
 const music = document.getElementById("bg-music");
 const btn = document.getElementById("btn-music");
 
-// ✅ Volume par défaut (entre 0.0 et 1.0)
-  music.volume = 0.2; // 20% (tu peux mettre 0.1, 0.15, 0.25...)
+// Volume par défaut (entre 0.0 et 1.0)
+  music.volume = 0.1; // (tu peux mettre 0.1, 0.15, 0.25...)
 
 btn.addEventListener("click", () => {
     if (music.paused) {
